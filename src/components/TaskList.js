2056bb84703c158d611377724d7ab11d71830a97
GraceBurke-88 <70772404+GraceBurke-88@users.noreply.github.com>
2022-04-18 20:40:03 -0400

    Getting ToDo tasks to render in list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Row, Container, Card, Col} from 'react-bootstrap';
 
 import TaskItem from './TaskItem';
 
 class TaskList extends React.Component {
-
+/*
   markDone = (task) => {
     const taskIndex = this.props.tasks.findIndex(t => t.id === task.id);
     let taskList = this.props.tasks;
@@ -13,7 +14,7 @@ class TaskList extends React.Component {
     this.props.onUpdateTaskList(taskList);
   }
 
-
+*/
   render() {
     const taskItems = this.props.tasks.map(task => {
       return <TaskItem task={task} key={task.id} markDone={this.markDone} />
@@ -24,12 +25,17 @@ class TaskList extends React.Component {
 
    // {console.log(taskItems)}
 
-    let counter = 0;
+    //let counter = 0;
+    let tasksYES = [];
     for (const obj of taskItems) {
-      console.log(obj)
-      if (obj.key === '1') counter++;
+      //console.log(obj.props.task.column)
+      if (obj.props.task.column === 'todo') {
+        tasksYES.push(obj);
+        console.log(tasksYES)
+        //counter++;
+      }
     }
-    console.log(counter); // 6
+    //console.log(counter); // 6
 
     //const donetaskItems = this.props.tasks.map(task => {
      // return <TaskItem task={task} key={task.id} markDone={this.markDone} />
@@ -37,9 +43,40 @@ class TaskList extends React.Component {
 
     return (
       <ul className="task-list list-group">
-       { taskItems }
+        <div>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg">Column 1<br/>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the bulk of
+                      the card's content.
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                  </Card.Body>
+                </Card>
+
+                {tasksYES}</div>
+              <div className="col-lg">Column 2<br/>
+                {'hello1'}</div>
+              <div className="col-lg">Column 3<br/>
+                {'hello2'}</div>
+              <div className="col-lg">Column 4<br/>
+                {'hello2'}</div>
+            </div>
+
+
+
+          </div>
+        </div>
 
       </ul>
+
+
 
     )
   }
