8a699bbfda7eb1510c424bbe0b3c7eb2dcf86767
GraceBurke-88 <70772404+GraceBurke-88@users.noreply.github.com>
2022-04-18 20:57:54 -0400

    Getting ToDo tasks to render in list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,80 +4,49 @@ import {Row, Container, Card, Col} from 'react-bootstrap';
 import TaskItem from './TaskItem';
 
 class TaskList extends React.Component {
-/*
-  markDone = (task) => {
-    const taskIndex = this.props.tasks.findIndex(t => t.id === task.id);
-    let taskList = this.props.tasks;
-    //console.log(taskList[1]);
-    taskList.splice(taskIndex, 1);
-    //console.log(this.props);
-    this.props.onUpdateTaskList(taskList);
-  }
-
-*/
   render() {
     const taskItems = this.props.tasks.map(task => {
       return <TaskItem task={task} key={task.id} markDone={this.markDone} />
     });
-    //const task1 = taskItems[1];
-    //const task2 = taskItems.some(task => task.id >= 5);
-
-
-   // {console.log(taskItems)}
-
-    //let counter = 0;
-    let tasksYES = [];
+    let tasksTODO = [];
+    let InProgress = [];
+    let InReview = [];
+    let Done = [];
     for (const obj of taskItems) {
-      //console.log(obj.props.task.column)
       if (obj.props.task.column === 'todo') {
-        tasksYES.push(obj);
-        console.log(tasksYES)
-        //counter++;
+        tasksTODO.push(obj);
+        console.log(tasksTODO);
       }
-    }
-    //console.log(counter); // 6
+      else if (obj.props.task.column === 'in-progress') {
+        InProgress.push(obj);
+      }
+      else if (obj.props.task.column === 'review') {
+        InReview.push(obj);
+      }
+      else if (obj.props.task.column === 'done') {
+        Done.push(obj);
+      }
 
-    //const donetaskItems = this.props.tasks.map(task => {
-     // return <TaskItem task={task} key={task.id} markDone={this.markDone} />
-    //});
+    }
 
     return (
       <ul className="task-list list-group">
         <div>
           <div className="container">
             <div className="row">
-              <div className="col-lg">Column 1<br/>
-                <Card style={{ width: '18rem' }}>
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                    <Card.Text>
-                      Some quick example text to build on the card title and make up the bulk of
-                      the card's content.
-                    </Card.Text>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
-                  </Card.Body>
-                </Card>
-
-                {tasksYES}</div>
-              <div className="col-lg">Column 2<br/>
-                {'hello1'}</div>
-              <div className="col-lg">Column 3<br/>
-                {'hello2'}</div>
-              <div className="col-lg">Column 4<br/>
-                {'hello2'}</div>
+              <div className="col-lg"><h3>ToDo</h3><br/>
+                { tasksTODO }</div>
+              <div className="col-lg"><h3>In-Progress</h3><br/>
+                { InProgress }</div>
+              <div className="col-lg"><h3>Review</h3><br/>
+                { InReview }</div>
+              <div className="col-lg"><h3>Done</h3><br/>
+                { Done }</div>
             </div>
-
-
-
           </div>
         </div>
 
       </ul>
-
-
-
     )
   }
 }
