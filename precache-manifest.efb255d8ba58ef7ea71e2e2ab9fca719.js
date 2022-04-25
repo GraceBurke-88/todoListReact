7356b8dd43bae723d39e0335ef05126e9b6c9b81
GraceBurke-88 <70772404+GraceBurke-88@users.noreply.github.com>
2022-04-25 12:55:56 -0400

    Updates

---
+++ precache-manifest.efb255d8ba58ef7ea71e2e2ab9fca719.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fd6374e76bd43ff0834",
+    "url": "/todoListReact/static/js/main.c44beec5.chunk.js"
+  },
+  {
+    "revision": "a9399c68d3527f9c8962",
+    "url": "/todoListReact/static/js/runtime~main.5576b137.js"
+  },
+  {
+    "revision": "92610468a55462e90093",
+    "url": "/todoListReact/static/js/2.538b53ae.chunk.js"
+  },
+  {
+    "revision": "00e7fd0a9d4ff80988f482bb195ba71d",
+    "url": "/todoListReact/index.html"
+  }
+];
